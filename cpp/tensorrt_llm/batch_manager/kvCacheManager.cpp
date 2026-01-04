7ca8ff668e08a5e1253814364a2df3eed7e20f28
Yi Sun <yisun0618@gmail.com>
2026-01-04 14:51:52 +0000

    feat: return KV cache utilization in perf_metrics
    Signed-off-by: Yi Sun <yisun0618@gmail.com>

--- cpp/tensorrt_llm/batch_manager/kvCacheManager.cpp
+++ cpp/tensorrt_llm/batch_manager/kvCacheManager.cpp
@@ -2656,6 +2656,8 @@ bool KVCacheManager::addSequence(
         llmRequest->updateAllocNewBlocksPerRequest(mBlockManager.getNumAllocNewBlocks() - numAllocNewBlocksPreRequest);
         llmRequest->updateReusedBlocksPerRequest(mBlockManager.getNumReusedBlocks() - numReusedBlocksPreRequest);
         llmRequest->updateMissedBlocksPerRequest(mBlockManager.getNumMissedBlocks() - numMissedBlocksPreRequest);
+        // Update system-level KV cache statistics for this request.
+        llmRequest->setKvCacheSystemStats(getMaxNumBlocks(), getUsedNumBlocks(), getNumFreeBlocks());
     }
 
     return true;
